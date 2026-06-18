92c2b1e68063929282a4e24ac013783224e4c3c2
Shruti Priya <shrutipriya.dev@gmail.com>
2026-06-18 17:01:26 +0530

    Update exam_type.php

--- exam_type.php
+++ exam_type.php
@@ -247,6 +247,7 @@ input[type="submit"]:hover, input[type="button"]:hover {
 
 echo '<form name=f method=post action=exam_type.php>
 <body>
+<h2 align="center">EXAM TYPE</h2>
 <table border=1 align=center>
 <tr>
     <td>Exam type ID</td>
