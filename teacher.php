ed0b34f9e127c377c5b2d36639eb1b4875b8b020
Shruti Priya <shrutipriya.dev@gmail.com>
2026-06-18 17:02:47 +0530

    Update teacher.php

--- teacher.php
+++ teacher.php
@@ -280,6 +280,7 @@ input[type="submit"]:hover, input[type="button"]:hover {
 
 echo '<form name=f method=post action=teacher.php>
 <body>
+<h2 align="center">TEACHER</h2>
 <table border=1 align=center>
 <tr>
     <td>Teacher ID</td>
